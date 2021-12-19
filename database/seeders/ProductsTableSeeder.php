692ecdc3b7d4afe85393ebf7b861b68ee8ae4890
Joy Das <joydascsepuc@gmail.com>
2021-12-19 22:08:12 +0600

    defining the images for the voyager

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -23,6 +23,7 @@ class ProductsTableSeeder extends Seeder
               'slug' => 'laptop-'.$i,
               'details' => [13,14,15][array_rand([13,14,15])] . ' inch, ' . [1, 2, 3][array_rand([1, 2, 3])] .' TB SSD, 32GB RAM',
               'price' => rand(149999, 249999),
+              'image' => 'products\December2021\laptop-'.$i,
               'description' =>'Lorem '. $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
           ])->categories()->attach(1);
       }
@@ -38,6 +39,7 @@ class ProductsTableSeeder extends Seeder
               'slug' => 'desktop-' . $i,
               'details' => [24, 25, 27][array_rand([24, 25, 27])] . ' inch, ' . [1, 2, 3][array_rand([1, 2, 3])] . ' TB SSD, 32GB RAM',
               'price' => rand(249999, 449999),
+              'image' => 'products\December2021\desktop-'.$i,
               'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
           ])->categories()->attach(2);
       }
@@ -49,6 +51,7 @@ class ProductsTableSeeder extends Seeder
               'slug' => 'phone-' . $i,
               'details' => [16, 32, 64][array_rand([16, 32, 64])] . 'GB, 5.' . [7, 8, 9][array_rand([7, 8, 9])] . ' inch screen, 4GHz Quad Core',
               'price' => rand(79999, 149999),
+              'image' => 'products\December2021\phone-'.$i,
               'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
           ])->categories()->attach(3);
       }
@@ -60,6 +63,7 @@ class ProductsTableSeeder extends Seeder
               'slug' => 'tablet-' . $i,
               'details' => [16, 32, 64][array_rand([16, 32, 64])] . 'GB, 5.' . [10, 11, 12][array_rand([10, 11, 12])] . ' inch screen, 4GHz Quad Core',
               'price' => rand(49999, 149999),
+              'image' => 'products\December2021\tablet-'.$i,
               'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
           ])->categories()->attach(4);
       }
@@ -71,6 +75,7 @@ class ProductsTableSeeder extends Seeder
               'slug' => 'tv-' . $i,
               'details' => [46, 50, 60][array_rand([7, 8, 9])] . ' inch screen, Smart TV, 4K',
               'price' => rand(79999, 149999),
+              'image' => 'products\December2021\tv-'.$i,
               'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
           ])->categories()->attach(5);
       }
@@ -82,6 +87,7 @@ class ProductsTableSeeder extends Seeder
               'slug' => 'camera-' . $i,
               'details' => 'Full Frame DSLR, with 18-55mm kit lens.',
               'price' => rand(79999, 249999),
+              'image' => 'products\December2021\camera-'.$i,
               'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
           ])->categories()->attach(6);
       }
@@ -93,6 +99,7 @@ class ProductsTableSeeder extends Seeder
               'slug' => 'appliance-' . $i,
               'details' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolorum!',
               'price' => rand(79999, 149999),
+              'image' => 'products\December2021\appliance-'.$i,
               'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
           ])->categories()->attach(7);
       }
--- resources/views/partials/might-like.blade.php
+++ resources/views/partials/might-like.blade.php
@@ -4,7 +4,7 @@
         <div class="might-like-grid">
             @foreach ($mightLike as $key => $product)
               <a href="{!! route('shop.show', $product->slug) !!}" class="might-like-product">
-                  <img src="{!! asset('img/products/'.$product->slug.'.jpg') !!}" alt="product">
+                  <img src="{{ asset('storage/'.$product->image) }}" alt="product">
                   <div class="might-like-product-name">{{ $product->name }}</div>
                   <div class="might-like-product-price">{{ $product->price / 100 }}</div>
               </a>
